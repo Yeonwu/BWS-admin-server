31944f6edc263eb4e5150a9ca3173085bb1694fe
Yeonwu Oh <yeonwu319@gmail.com>
2022-08-05 16:23:03 +0900

    Change firebase config file path absolute

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,11 +1,13 @@
 import firebaseConf from "../config/firebaseConf.js";
 import admin from "firebase-admin";
 import { getFirestore } from "firebase-admin/firestore";
+import path from "path";
 
-const serviceAccount = firebaseConf.serviceAccountKeyPath;
+const serviceAccount = firebaseConf.serviceAccountKeyFileName;
+const accountPath = path.resolve(`./config/${serviceAccount}`);
 
 const app = admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(accountPath),
 });
 
 const db = getFirestore();
